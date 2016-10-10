7dc93abb917493fdb55dc49393444c3870981c75
lolzzlolzz <phengsengvuthy@gmail.com>
2016-10-10 23:42:34 +0700

    Added a new activity

---
+++ app/src/main/java/theara/com/testgit/thy.java
@@ -0,0 +1,13 @@
+package theara.com.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class thy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thy);
+    }
+}
