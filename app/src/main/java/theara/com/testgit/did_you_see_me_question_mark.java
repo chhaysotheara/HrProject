3e9334a368769c99a27036d85908ee69d0b7fb46
lolzzlolzz <phengsengvuthy@gmail.com>
2016-10-11 09:42:53 +0700

    Created a layout

---
+++ app/src/main/java/theara/com/testgit/did_you_see_me_question_mark.java
@@ -0,0 +1,13 @@
+package theara.com.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class did_you_see_me_question_mark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_did_you_see_me_question_mark);
+    }
+}
